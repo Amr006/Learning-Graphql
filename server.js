8f7ef97be52959072f61b72e50343d6e070b03b8
Amr006 <2021170374@cis.asu.edu.eg>
2023-08-07 13:19:48 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,122 @@
+//const express =require('express')
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import db from "./_db.js";
+
+import {typeDefs} from "./schema.js";
+
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games
+    },
+    game(_,args)
+    {
+      return db.games.find((e) => 
+        e.id === args.id
+      
+      )
+    },
+    authors()
+    {
+      return db.authors
+    }
+    ,
+    author(_,args)
+    {
+      return db.authors.find((e) => e.id === args.id)
+    }
+    ,
+    reviews()
+    {
+      return db.reviews
+    },
+    review(_,args)
+    {
+      return db.reviews.find((e) => e.id === args.id)
+    }
+    
+
+  }
+  ,
+  Game: {
+    reviews(parent)
+    {
+      return db.reviews.filter((e) => e.game_id === parent.id)
+    }
+  }
+  ,
+  Review:
+  {
+    game(parent){
+      return db.games.find((e) => e.id === parent.game_id)
+    },
+    author(parent)
+    {
+      return db.authors.find((e) => e.id === parent.author_id)
+    }
+  },
+  Author:
+  {
+    reviews(parent)
+    {
+      return db.reviews.filter((e) => e.author_id === parent.id)
+    }
+  },
+  Mutation: {
+    addGame(_,args){
+      let game = {
+        ...args.game ,
+        id: Math.floor(Math.random() * 10000).toString()
+      }
+      db.games.push(game)
+
+      return game 
+    }
+    ,
+    deleteGame(_,args)
+    {
+      db.games = db.games.filter((g) => g.id !== args.id)
+
+      return db.games
+    }
+    ,
+    updateGame(_,args)
+    {
+      db.games = db.games.map((g) => {
+        if(g.id === args.id)
+        {
+          return {...g , ...args.edits}
+        }
+
+      })
+
+      return db.games.find((e) => g.id === args.id)
+    }
+  }
+
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  
+})
+
+
+
+const { url } = await startStandaloneServer(server , {
+  listen : {port:4000}
+})
+
+
+
+
+// const app = express()
+
+// app.listen(5000 , () => {
+//   console.log('server running')
+  
+// }
+// )
